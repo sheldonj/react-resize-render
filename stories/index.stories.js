69997629750a29a2af8d12a2af20d3f1a288fd9f
James Sheldon <jsheldon@criticalmass.com>
2018-05-29 22:24:08 -0600

    fix: remoed storybook entires that didnt apply to this repo

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,23 +1,7 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import RenderResize from '../modules/RenderResize';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
 storiesOf('RenderResize', module)
   .add('basic example', () => (
     <RenderResize
@@ -36,7 +20,7 @@ storiesOf('RenderResize', module)
         render={dimensions => {
           return (
             <div style={{}}>
-             😀 {dimensions.width} - {dimensions.height}
+              😀 {dimensions.width} - {dimensions.height}
             </div>
           );
         }}
@@ -45,7 +29,7 @@ storiesOf('RenderResize', module)
         render={dimensions => {
           return (
             <div style={{}}>
-             😎 {dimensions.width} - {dimensions.height}
+              😎 {dimensions.width} - {dimensions.height}
             </div>
           );
         }}
